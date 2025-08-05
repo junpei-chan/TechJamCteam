6b81e76a416fb8626ac1f9f3095cbf5e02a56ddf
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-05 14:11:14 +0900

    main.py 追加

--- back/api/main.py
+++ back/api/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from .database import engine, Base
-from .routers import menu
+from .routers import menu, shop
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Menu API", version="1.0.0")
 
 app.include_router(menu.router)
+app.include_router(shop.router)
 
 @app.get("/")
 def read_root():
