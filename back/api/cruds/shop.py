eb6bff66c5a0d55ae119a0890bc8451a2ae7f7eb
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-05 13:43:45 +0900

    cruds update, delete 追加

--- back/api/cruds/shop.py
+++ back/api/cruds/shop.py
@@ -16,3 +16,26 @@ def get_shops(db: Session, skip: int = 0, limit: int = 100):
 # 特定のIDの店舗を一件取得
 def get_shop_by_id(db: Session, shop_id: int):
   return db.query(models.Shop).filter(models.Shop.shop_id == shop_id).first()
+
+# 店舗情報の更新
+def update_shop(db: Session, shop_id, shop_update: schemas.ShopCreate):
+  db_shop = db.query(models.Shop).filter(models.Shop.shop_id == shop_id).first()
+  if not db_shop:
+    return None   # NotFound対応
+  
+  for key, value in shop_update.dict().items():
+    setattr(db_shop, key, value)    # モデルの各属性に新しい値を代入
+
+  db.commit()
+  db.refresh(db_shop)
+  return db_shop
+
+# 店舗情報の削除
+def delete_shop(db: Session, shop_id: int):
+    db_shop = db.query(models.Shop).filter(models.Shop.shop_id == shop_id).first()
+    if not db_shop:
+        return None
+
+    db.delete(db_shop)
+    db.commit()
+    return db_shop
