a65893b348bf0916f23f6a07b571a12971a383ef
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-05 13:50:53 +0900

    routers update, delete 追加

--- back/api/routers/shop.py
+++ back/api/routers/shop.py
@@ -15,10 +15,26 @@ def create_shop(shop: schemas.ShopCreate, db: Session = Depends(get_db)):
 def read_shops(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
   return crud.get_shops(db, skip, limit)
 
-# GET /shops/1 などで1件のショップを取得（見つからなければ404）
+# /shops/1 などで1件のショップを取得
 @router.post("/", response_model=schemas.ShopRead)
 def read_shop(shop_id: int, db: Session = Depends(get_db)):
   shop = crud.get_shop_by_id(db, shop_id)
   if not shop:
     raise HTTPException(status_code=404, detail="Shop not found")
   return shop
+
+# /shops/{shop_id} にアクセスで、該当IDのショップが更新される
+@router.put("/{shop_id}", response_model=schemas.ShopRead)
+def update_shop(shop_id: int, shop: schemas.ShopCreate, db: Session = Depends(get_db)):
+    updated_shop = crud.update_shop(db, shop_id, shop)
+    if updated_shop is None:
+        raise HTTPException(status_code=404, detail="Shop not found")
+    return updated_shop
+
+# /shops/{shop_id} でショップを削除
+@router.delete("/{shop_id}", response_model=schemas.ShopRead)
+def delete_shop(shop_id: int, db: Session = Depends(get_db)):
+    deleted_shop = crud.delete_shop(db, shop_id)
+    if deleted_shop is None:
+        raise HTTPException(status_code=404, detail="Shop not found")
+    return deleted_shop
