24ca0865c8a48fabeefcfe1db4aa9e2ecc16d979
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-07 20:25:19 +0900

    models 修正

--- back/api/models/notification.py
+++ back/api/models/notification.py
@@ -7,12 +7,12 @@ class Notification(Base):
   __tablename__ = "notifications"
 
   id = Column(Integer, primary_key=True, index=True)
-  user_id = Column(Integer, ForeignKey("users.id"))                          # 通知の送り先
+  user_id = Column(Integer, ForeignKey("users.id"), nullable=False)                          # 通知の送り先
   shop_id = Column(Integer, ForeignKey("shops.id"), nullable=True)           # 関連する店舗
   shop_user_id = Column(Integer, ForeignKey("shop_users.id"), nullable=True) # 店舗側ユーザー（オプション）
   contents = Column(String, nullable=False)                                  # 通知内容
   created_at = Column(DateTime(timezone=True), server_default=func.now())
-  status = Column(String)                                                    # 既読・未読
+  status = Column(String, default="unread")                                                    # 既読・未読
 
   user = relationship("Users")
   shop = relationship("Shop")
