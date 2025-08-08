b3d97f92d63eb13d51f8e118175e5950eff38b91
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-08 12:37:10 +0900

    users models  修正

--- back/api/models/notification_users.py
+++ back/api/models/notification_users.py
@@ -7,8 +7,8 @@ class NotificationUsers(Base):
   __tablename__ = "notification__users"
 
   id = Column(Integer, primary_key=True, index=True)
-  notification_id = Column(Integer, ForeignKey("notifications.id", ondelete="CASCADE"), nullable=False)
-  user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+  notification_id = Column(Integer, ForeignKey("notifications.notification_id", ondelete="CASCADE"), nullable=False)
+  user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
 
   created_at = Column(DateTime(timezone=True), server_default=func.now())
 
