235a7d4ae988ee7b342217551a6cef54a2b4413f
wkbys <wakabayashi.m.24kdgn@gmail.com>
2025-08-08 10:40:48 +0900

    users models 作成

---
+++ back/api/models/notification_users.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from database import Base
+
+class NotificationUsers(Base):
+  __tablename__ = "notification__users"
+
+  id = Column(Integer, primary_key=True, index=True)
+  user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+  notification_id = Column(Integer, ForeignKey("notification.id", ondelete="CASCADE"), nullable=False)
+
+  created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+  user = relationship("Users")
+  notification = relationship("Notification")
+
+  def __repr__(self):
+    return f"<NotificationUsers(notification_id={self.notification_id}, user_id={self.user_id})>"
